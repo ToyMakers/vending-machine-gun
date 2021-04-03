d61ec0ef8b72008e507adb81004ac7c666ce31c3
geon <ehdrjs4046@gmail.com>
2021-04-03 14:46:17 +0900

    [ Add ] : dev pile up

--- src/components/layout/WalletBasket/Basket.tsx
+++ src/components/layout/WalletBasket/Basket.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
-import Can from '../../common/Can';
+import { isLabeledStatement } from 'typescript';
+import { sodaType } from '../../../asset/brand';
+import { RootState } from '../../../modules';
+import Can, { propsType } from '../../common/Can';
 import { WalletBasketTitle } from '../../common/WalletBasketTitle';
 
 const BasketWrap = styled.div`
@@ -13,6 +17,9 @@ const BasketTitle = styled(WalletBasketTitle)`
 `;
 
 const BasketBx = styled.ul`
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
     width: 100%;
     height: 400px;
     padding: 20px;
@@ -21,7 +28,7 @@ const BasketBx = styled.ul`
 `;
 
 const CanItem = styled.li`
-    width: 25%;
+    width: 33.3333%;
 `;
 
 const CanNumber = styled.span`
@@ -36,14 +43,31 @@ const CanNumber = styled.span`
 `;
 
 function Basket() {
+    const basketSoda = useSelector((state: RootState) => state.picking.basket);
+
+    const label = basketSoda.map((soda) => soda.label);
+
+    const init: { [index: string]: number } = {};
+    const overlapObj = label.reduce((acc, cur) => {
+        acc[cur] = (acc[cur] || 0) + 1;
+        return acc;
+    }, init);
+
+    const overlapList = Object.entries(overlapObj);
     return (
         <BasketWrap>
             <BasketTitle>BASKET</BasketTitle>
             <BasketBx>
-                <CanItem>
-                    <Can />
-                    <CanNumber>X {0}</CanNumber>
-                </CanItem>
+                {overlapList.map((soda) => {
+                    const filtSoda = basketSoda.filter((curSoda) => curSoda.label === soda[0]);
+                    const { id, product, color } = filtSoda[0];
+                    return (
+                        <CanItem key={soda[0]}>
+                            <Can key={id} product={product} color={color} />
+                            <CanNumber>X {soda[1]}</CanNumber>
+                        </CanItem>
+                    );
+                })}
             </BasketBx>
         </BasketWrap>
     );
